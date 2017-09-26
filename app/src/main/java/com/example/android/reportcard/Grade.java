8206819beb256ba7836d3739baa213257a75fb75
Marvin Ramirez <mramirez@constantevolve.ca>
2017-09-26 07:24:51 -0400

    first commit

---
+++ app/src/main/java/com/example/android/reportcard/Grade.java
@@ -0,0 +1,28 @@
+package com.example.android.reportcard;
+
+/**
+ * Created by marvinramirez on 2017-09-25.
+ */
+
+
+public class Grade {
+
+    private String mDefaultGrade;
+
+    private String mDefaultClass;
+
+    public Grade(String defaultGrade, String defaultClass){
+        mDefaultGrade = defaultGrade;
+        mDefaultClass = defaultClass;
+    }
+
+    public String getDefaultGrade () {
+        return mDefaultGrade;
+    }
+
+    public String getDefaultClass () {
+        return mDefaultClass;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/android/reportcard/ReportCard.java
@@ -0,0 +1,36 @@
+package com.example.android.reportcard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ReportCard extends AppCompatActivity {
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            //creare array of words
+            ArrayList<Grade> grades = new ArrayList<Grade>();
+
+            grades.add(new Grade("A", "Math"));
+            grades.add(new Grade("A+", "Science"));
+            grades.add(new Grade("B", "English"));
+            grades.add(new Grade("B-", "Art"));
+
+
+            GradeAdapter adapter =
+                    new GradeAdapter(this, grades);
+
+            ListView listView = (ListView) findViewById(R.id.list);
+
+            listView.setAdapter(adapter);
+
+
+        }
+
+}
